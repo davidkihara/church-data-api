836139be63988786670f93563339b9f1091d3485
Melvin Otieno <o.melvinotieno@gmail.com>
2021-10-06 00:02:07 +0300

    display navigation

--- resources/views/register_church.blade.php
+++ resources/views/register_church.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Church</title>
-    
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container mt-5">
+@section('content')
+    @parent
+
+    <div class="container">
         <!-- Success message -->
         @if(Session::has('success'))
             <div class="alert alert-success">
@@ -37,8 +30,9 @@
                 <input type="text" class="form-control" name="registration_number" id="registration_number">
             </div>
 
+            <input name="user_id" value="{{Auth::user()->id}}" type="hidden">
+
             <input type="submit" name="send" value="Submit" class="btn btn-dark btn-block">
         </form>
     </div>
-</body>
-</html>
+@endsection

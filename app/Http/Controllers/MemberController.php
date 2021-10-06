a82abfd269c50848a7ba767c8bed052205513a6a
Melvin Otieno <o.melvinotieno@gmail.com>
2021-10-06 08:45:21 +0300

    viewMember

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Member;
 
 class MemberController extends Controller
 {
+    public function viewMember($id) {
+        $member = DB::table('members')->where('id', $id)->first();
+        $member_id = $member->id;
+        return view('member', ['member' => $member,]);
+    }
+
     public function createForm(Request $request) {
-        return view('register_member');
+        $user_id = Auth::user()->id;
+        $churches = DB::table('churches')->where('user_id', $user_id)->get();
+        return view('register_member', ['churches' => $churches]);
     }
 
     public function MemberForm(Request $request) {
@@ -16,11 +26,11 @@ class MemberController extends Controller
             'email' => 'required',
             'phone_number' => 'required',
             'social_security_number' => 'required',
+            'church_id' => 'required'
          ]);
 
         Member::create($request->all());
 
-        // 
         return back()->with('success', 'Member Registered.');
     }
 }
